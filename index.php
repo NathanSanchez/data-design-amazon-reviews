23ea85a412214b5ec0808969421d2f7beb334d48
Nathan Sanchez <nsanchez121@cnm.edu>
2016-10-12 13:13:52 -0600

    w.i.p part 7

--- index.php
+++ index.php
@@ -8,10 +8,10 @@
 		<h1>Tom Crops</h1>
 
 		<p>mainly people who buy things on amazon and want some reviews for the products there buying. My persona is gonna be named Tom Crops Tom is a a young man who wants to buy things on amazon,Tom is 22 5'8 avreage looks ,single and ready to mingle. Tom is usally chill but sometimes he gets a little worked up and breaks his keyboard(main reason hes on amazon so much)</p>
-		<h2>use case</h2>
+		<h1>use case</h1>
 		<p>User looking for some shoes, finds shoes, user not sure if shoes and high quality checks reviews,buys shoes, then writes a review of how sexy these shoes are </p>
 		<h1>identify entities</h1>
-		<p></p>
+		<p>comment section 1-5 star button user profile name user profile picture</p>
 	</body>
 
 
