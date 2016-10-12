345236b0c860b1032e2c24f596494bf6b574bbff
Nathan Sanchez <nsanchez121@cnm.edu>
2016-10-12 13:00:06 -0600

    w.i.p part 4

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
-<html>
+<html lang="en">
 	<head>
-		<meta charset="UTF-8">
+		<meta charset="UTF-8"/>
 		<title>data design for amazon reviews baby</title>
 	</head>
 	<body>
-		<section>
-			<h1>Tom Crops</h1>
-			</section>
+		<header>Nathan</header>
+		<h1>Tom Crops</h1>
+
 		<p>mainly people who buy things on amazon and want some reviews for the products there buying. My persona is gonna be named Tom Crops Tom is a a young man who wants to buy things on amazon,Tom is 22 5'8 avreage looks ,single and ready to mingle. Tom is usally chill but sometimes he gets a little worked up and breaks his keyboard(main reason hes on amazon so much)</p>
-		<!-- use case-->
 		<p>User looking for some shoes, finds shoes, user not sure if shoes and high quality checks reviews,buys shoes, then writes a review of how sexy these shoes are </p>
 
 	</body>
