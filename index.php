95fa2c06ea6f288b9a961b053453bfe0034d47db
Nathan Sanchez <nsanchez121@cnm.edu>
2016-10-12 11:55:28 -0600

    w.i.p <!--nathan2-->

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	<body>
 		<p>mainly people who buy things on amazon and want some reviews for the products there buying. My persona is gonna be named Tom Crops Tom is a a young man who wants to buy things on amazon,Tom is 22 5'8 avreage looks ,single and ready to mingle. Tom is usally chill but sometimes he gets a little worked up and breaks his keyboard(main reason hes on amazon so much)</p>
 		<p>User looking for some shoes, finds shoes, user not sure if shoes and high quality checks reviews,buys shoes, then writes a review of how sexy these shoes are </p>
-<!-- test-->
+<!-- test2-->
 	</body>
 
 
