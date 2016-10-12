31f8e28489cb3521f46751aaa47b9c9d02e426f6
Nathan Sanchez <nsanchez121@cnm.edu>
2016-10-12 11:49:43 -0600

    w.i.p

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>data design for amazon reviews baby</title>
+	</head>
+	<body>
+		<p>mainly people who buy things on amazon and want some reviews for the products there buying. My persona is gonna be named Tom Crops Tom is a a young man who wants to buy things on amazon,Tom is 22 5'8 avreage looks ,single and ready to mingle. Tom is usally chill but sometimes he gets a little worked up and breaks his keyboard(main reason hes on amazon so much)</p>
+		<p>User looking for some shoes, finds shoes, user not sure if shoes and high quality checks reviews,buys shoes, then writes a review of how sexy these shoes are </p>
+
+	</body>
+
+
+
+
+
+
+
+
+</html>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nathansanchez
+ * Date: 10/12/16
+ * Time: 11:27 AM
+ */
