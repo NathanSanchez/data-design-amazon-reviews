5a4514f1008e50e07af624506793f544c0361527
Nathan Sanchez <nsanchez121@cnm.edu>
2016-10-13 13:18:27 -0600

    w.i.p part 15

--- index.php
+++ index.php
@@ -8,20 +8,32 @@
 		<h1>Tom Crops</h1>
 		<img src="img/wild-man-looking-confused-banana-28747201.jpg" alt="Tom Crops"/>
 		<p>mainly people who buy things on amazon and want some reviews for the products there buying. My persona is gonna be named Tom Crops Tom is a a young man who wants to buy things on amazon,Tom is 22 5'8 avreage looks ,single, Tom manily uses his home pc his internet provieder is comcast :c he sometime uses his phone to shop around on amamazon. Tom works hard and has bought so many things on amazon and left so many reviews that amazon made him a prproduct tester and offical reviewer </p>
+
 		<h1>use case</h1>
+
 		<p>Tom looking for some shoes, gose to amazon.com gose to search bar types in cool shoes 876 results are found. Tom starts scrolling down the web page looking at all the cool shoes finds a pair of shoes he likes double clicks on them decied that the shoes are right for him adds them to cart clicks on payment all the credit card info boxes pop up he enters his card info then he adds his address with toms amazon prime account he gets 2 day free shipping. fast forword 2 days and tom gets his new kicks hes pretty hype about them puts them on instantly he feels like a new man tom feels so good he gose back to the page for his cool shoes and scrolls down to the review section begains typing out one of toms hella good reviews leaves a 5 star rating </p>
+
+		<h1>action list</h1>
+
 		<h1>identify entities</h1>
-		<!--if it looks like i dont know what im doing well your right cause i dont 3-->
+		<!--if it looks like i dont know what im doing well your right cause i dont 4-->
 		<p>comment section 1-5 star button user profile name </p>
+
 		<h1>conceptual model</h1>
 		<ul>
+
 			<li>productid (primary)</li>
 			<li>productInfo</li>
 			<li>userProfileid</li>
 			<li>userName</li>
+			<li>userPicture</li>
+			<li>reviewid</li>
 			<li>reviewStars</li>
 			<li>reviewContent</li>
 			<li>reviewDateTime</li>
+			<li>productid has a many to many relationship</li>
+			<li>reviewid has a 1 to many relationships</li>
+
 		</ul>
 	</body>
 
