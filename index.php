b67297e549571ce1fef8e6b2c65391b86b3f60f8
Nathan Sanchez <nsanchez121@cnm.edu>
2016-10-13 15:14:25 -0600

    w.i.p part 17

--- index.php
+++ index.php
@@ -7,15 +7,14 @@
 		<header>Nathan is one cool dude - nathan sanchez</header>
 		<h1>Tom Crops</h1>
 		<img src="img/wild-man-looking-confused-banana-28747201.jpg" alt="Tom Crops"/>
-		<p>mainly people who buy things on amazon and want some reviews for the products there buying. My persona is gonna be named Tom Crops Tom is a a young man who wants to buy things on amazon,Tom is 22 5'8 avreage looks ,single, Tom manily uses his home pc his internet provieder is comcast :c he sometime uses his phone to shop around on amamazon. Tom works hard and has bought so many things on amazon and left so many reviews that amazon made him a prproduct tester and offical reviewer </p>
+		<p> My persona is gonna be named Tom Crops Tom is a a young man who wants to buy things on amazon,Tom is 22 5'8 avreage looks ,single, Tom manily uses his home pc his internet provieder is comcast :c he sometime uses his phone to shop around on amamazon. Tom works hard and has bought so many things on amazon and left so many reviews that amazon made him a prproduct tester and offical reviewer </p>
 
 		<h1>use case</h1>
 
-		<p>Tom Crops is looking </p>
-
+		<p>Tom Crops uses amazon to shop he loves shopping he can find anything on amazon he often looks for good deals on toys and such so he can send them to his sisters kids her kids dont have alot of toys and tom likes giving them toys because he likes to see them happy. sometimes Tom looks for new clothes for himself on amazon he always trys to find good deals on some sick new clothes which hes pretty good at.</p>
 		<h1>action list</h1>
 
-		<p>Tom looking for some shoes, goes to amazon.com gose to search bar types in cool shoes 876 results are found. Tom starts scrolling down the web page looking at all the cool shoes finds a pair of shoes he likes double clicks on them decied that the shoes are right for him adds them to cart clicks on payment all the credit card info boxes pop up he enters his card info then he adds his address with toms amazon prime account he gets 2 day free shipping. fast forword 2 days and tom gets his new kicks hes pretty hype about them puts them on instantly he feels like a new man tom feels so good he goes back to the page for his cool shoes and scrolls down to the review section begains typing out one of toms hella good reviews leaves a 5 star rating</p>
+		<p>Tom looking for some shoes, goes to amazon.com goes to search bar types in cool shoes 876 results are found. Tom starts scrolling down the web page looking at all the cool shoes finds a pair of shoes he likes double clicks on them decied that the shoes are right for him adds them to cart clicks on payment all the credit card info boxes pop up he enters his card info then he adds his address with toms amazon prime account he gets 2 day free shipping. fast forword 2 days and tom gets his new kicks hes pretty hype about them puts them on instantly he feels like a new man tom feels so good he goes back to the page for his cool shoes and scrolls down to the review section begains typing out one of toms hella good reviews leaves a 5 star rating</p>
 
 		<h1>identify entities</h1>
 		<!--if it looks like i dont know what im doing well your right cause i dont 5-->
@@ -34,9 +33,10 @@
 		<ul>
 
 			<li>productInfo</li>
+			<li>productPrice</li>
+			<li>productQuantity</li>
 			<li>profileName</li>
 			<li>profilePicture</li>
-			<li>reviewid</li>
 			<li>reviewStars</li>
 			<li>reviewContent</li>
 			<li>reviewDateTime</li>
@@ -45,9 +45,9 @@
 		<h1>relationships</h1>
 		<ul>
 
-			<li>productid has a many to many relationship</li>
-			<li>reviewid has a one to many relationships</li>
-			<li>profileid has a one to one</li>
+			<li>product has a many to many relationship</li>
+			<li>review has a many to many relationships</li>
+			<li>profile has a one to one</li>
 
 		</ul>
 
