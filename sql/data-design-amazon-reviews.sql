3290c0ebe0a664a26bfda5802cc8e176e03ff5c8
Nathan Sanchez <nsanchez121@cnm.edu>
2016-10-18 10:39:47 -0600

    w.i.p part 25

--- sql/data-design-amazon-reviews.sql
+++ sql/data-design-amazon-reviews.sql
@@ -1,14 +1,13 @@
 DROP TABLE IF EXISTS review;
 DROP TABLE IF EXISTS product;
-DROP TABLE IF EXISTS PROFILE;
+DROP TABLE IF EXISTS profile;
 
 
 CREATE TABLE profile (
 	profileId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	profileName VARCHAR(32) NOT NULL,
 	profileEmail VARCHAR(254) NOT NULL,
-	profilePicture
-	profileName
+	profilePicture INT UNSIGNED NOT NULL,
 	PRIMARY KEY (profileId)
 
 );
@@ -17,16 +16,16 @@ CREATE TABLE review (
 	reviewProfileId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	reviewDate DATETIME NOT NULL,
 	reviewContent VARCHAR (1000) NOT NULL,
-	reviewId INT UNSIGNED AUTO_INCREMENT NOT NULL,
-	INDEX (reviewProfileId),
+	reviewAuthorId INT UNSIGNED NOT NULL,
+	INDEX (reviewProfileId) ,
 	FOREIGN KEY (reviewProfileId) REFERENCES profile(profileId),
-	PRIMARY KEY (reviewId),
+	PRIMARY KEY (reviewProfileId)
 
 );
 
 CREATE TABLE product (
 	productId INT UNSIGNED AUTO_INCREMENT NOT NULL,
-	productPrice
+	productPrice INT UNSIGNED NOT NULL
 
 );
 -- kill the children first >;^(
